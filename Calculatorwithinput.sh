599f86da0e7462631af80ae1f3919f8cb6e2b10d
Zohaib Ahmed <zohaib4986@gmail.com>
2022-07-04 12:22:33 -0400

    files

---
+++ Calculatorwithinput.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Please enter first digit "
+read num1
+
+echo "Please enter second digit "
+read num2
+
+total=$((num1+num2))
+
+echo $total
--- Calculatot.sh
+++ Calculatot.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-read -p "Enter first digit : " num1
-read -p "Enter second digit : " num2
+read -p "Enter first digit: " num1
+read -p "Enter second digit: " num2
 
-total=(${num1}+${num2})
+total=$((num1+num2))
 
 echo ${total}
