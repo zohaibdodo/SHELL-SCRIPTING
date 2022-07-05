a239329a2ad31b1bc530c9d806575e72399f41d6
zohaibdodo <zohaib4986@gmail.com>
2022-07-05 16:44:17 +0500

    a

--- CALCULATOR/Calculatorwithroundbrackets(OK).sh
+++ CALCULATOR/Calculatorwithroundbrackets(OK).sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-read -p "Please enter first digit" num1
-read -p "Please enter second digit" num2
+read -p "Please enter first digit: " num1
+read -p "Please enter second digit: " num2
 
 total=$((${num1}+${num2}))
 
