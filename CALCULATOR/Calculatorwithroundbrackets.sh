e90fa37b2ec2e48ea382c7c0e0682a4d1b5d5360
zohaibdodo <zohaib4986@gmail.com>
2022-07-05 15:31:57 +0500

    Update Calculatorwithroundbrackets.sh

--- CALCULATOR/Calculatorwithroundbrackets.sh
+++ CALCULATOR/Calculatorwithroundbrackets.sh
@@ -3,6 +3,6 @@
 read -p "Please enter first digit" num1
 read -p "Please enter second digit" num2
 
-total=$((${num}1+${num2}))
+total=$((${num1}+${num2}))
 
 echo "The sum of given inputs is:" ${total}
